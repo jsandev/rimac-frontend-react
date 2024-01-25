52cb784c2c20ea95559d1bb7fd7cc6bff281be0a
Jimmy Sandoval <jsandovaldev.pe@gmail.com>
2024-01-24 22:02:50 -0500

    feat: Add plans

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Onboarding } from "./components/Onboarding";
+import { Plans } from "./components/Plans";
 
 import BlurPurple from "./assets/blur-purple.svg?react";
 import BlurGreen from "./assets/blur-green.svg?react";
@@ -16,6 +17,7 @@ function App() {
       </div>
       <Header />
       <main>
+        <Plans />
         <Onboarding />
         <Footer />
       </main>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,10 +4,22 @@ interface IProps {
   title: string;
   onClick: () => void;
   className?: string;
+  color?: "primary" | "secondary";
 }
-export const Button: React.FC<IProps> = ({ title, onClick }) => {
+export const Button: React.FC<IProps> = ({
+  title,
+  onClick,
+  color = "primary",
+}) => {
   return (
-    <button className={classNames("button", classNames)} onClick={onClick}>
+    <button
+      className={classNames(
+        "button",
+        color === "secondary" ? "button--secondary" : "",
+        classNames
+      )}
+      onClick={onClick}
+    >
       {title}
     </button>
   );
